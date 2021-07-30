c4713b219f0ae94c0e85a1618a755edbceedc0c1
KyungSeokChoi <ssingame@nate.com>
2021-07-30 14:50:32 +0900

    Vuepress 2차 진행중

--- docs/.vuepress/components/Articles.vue
+++ docs/.vuepress/components/Articles.vue
@@ -1,16 +1,17 @@
 <template>
   <div>
-    <a-timeline>
-      <a-timeline-item v-for="(item, index) in this.pages" v-bind:key="index">
-        <div>{{ item.lastUpdated }} {{ item.yyyyMMdd }}</div>
-        <a v-bind:href="item.path">{{ item.title }} </a>
-      </a-timeline-item>
-    </a-timeline>
+      <template v-for="(item, index) in this.pages">
+        <h3 v-bind:key="index">{{ item.yyyyMMdd }}</h3>
+        <a-anchor v-for="(page, index) in item.body" :affix="false" v-bind:key="index">
+          <a-anchor-link v-bind:key="key" v-bind:href="page.path" v-bind:title="page.title" />
+        </a-anchor>
+      </template>
   </div>
 </template>
 
 <script>
 import moment from 'moment';
+import * as R from 'ramda';
 
 
 export default {
@@ -28,7 +29,7 @@ export default {
   },
   computed: {
     pages() {
-      return this.$site.pages.map(({
+      const list = this.$site.pages.map(({
         frontmatter,
         headers,
         key,
@@ -42,7 +43,7 @@ export default {
         let hhMMss;
         if (lastUpdated) {
             const date = lastUpdated.split(",");
-            yyyyMMdd = date[0];
+            yyyyMMdd = moment(date[0], 'MM/DD/YYYY').format('YYYY-MM-DD');
             hhMMss = date[1];
         }
         return {
@@ -56,6 +57,22 @@ export default {
           regularPath,
           relativePath,
           title,
+        }
+      });
+      const result = R.groupBy(o => {
+        if (o.yyyyMMdd) {
+          return o.yyyyMMdd
+        } else {
+          return '2021-01-01'
+        }
+      })(list);
+      debugger;
+      return Object.keys(result)
+      .sort(o => o[0] < o[1])
+      .map(o => {
+        return {
+          yyyyMMdd: o,
+          body: result[o],
         }
       });
     },
